277f6f0039c6d0ed7a0bac96e220159493bbec84
Keshia Wijaya <pawspower@Keshias-MacBook-Pro.local>
2022-11-19 21:39:45 +1100

    sat 02

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+// import Img from "../img/img.png";
+// import Attach from "../img/attach.png";
 
 const Input = () => {
     return (
-        <div className='input'>Input</div>
+    <div className='input'>
+        <input type="text" placeholder='Type something...'/>
+        <div className='send'>
+            <img src="" alt="" />
+            <input type="file" style={{display:"none"}} id="file" />
+            <label htmlFor="file">
+                <img src="" alt="" />
+            </label>
+            <button>Send</button>
+        </div>
+    </div>
     )
 }
 
