42b4ad1e19323c855447b634cdd21c3fe58e4950
Keshia Wijaya <kesh.learning@gmail.com>
2022-11-21 23:00:47 +1100

    monday04

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
-// import Cam from "";
-// import Add from "";
-// import More from "";
-import Messages from './Messages'
-import Input from './Input'
+import React, { useContext } from "react";
+import Cam from "../img/cam.png";
+import Add from "../img/add.png";
+import More from "../img/more.png";
+import Messages from "./Messages";
+import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
 
-const Chats = () => {
-    return (
-        <div className='chat'>
-            <div className='chatInfo'>
-                <span>Jane</span>
-                <div className='chatIcons'>
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                </div>
-            </div>
-            <Messages />
-            <Input />
+const Chat = () => {
+  const { data } = useContext(ChatContext);
+
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>{data.user?.displayName}</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
         </div>
-    )
-}
+      </div>
+      <Messages />
+      <Input/>
+    </div>
+  );
+};
 
-export default Chats;
+export default Chat;
