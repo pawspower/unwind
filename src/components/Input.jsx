55222e7e4e8cdbaa5e867fa218a1b16e9aae1b76
Keshia Wijaya <kesh.learning@gmail.com>
2022-11-22 10:14:09 +1100

    test01

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,4 @@
 
-// import Img from "../img/img.png";
-// import Attach from "../img/attach.png";
 import React, { useContext, useState } from "react";
 import Img from "../img/img.png";
 import Attach from "../img/attach.png";
