897bcbf00d08f970f71845ef47264afac580ec23
Keshia Wijaya <kesh.learning@gmail.com>
2022-11-24 10:03:39 +1100

    wednesday01

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
-import Cam from "../img/cam.png";
-import Add from "../img/add.png";
-import More from "../img/more.png";
+import Cam from "../img/video-camera-alt.png";
+import Add from "../img/user-add.png";
+import More from "../img/list.png";
 import Messages from "./Messages";
 import Input from "./Input";
 import { ChatContext } from "../context/ChatContext";
